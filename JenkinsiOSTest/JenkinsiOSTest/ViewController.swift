e3590868919b68608150ef9c658c856c53d6dda8
Nithin Kumar <nkdiyasys@gmail.com>
2020-04-09 11:16:52 +0530

    Commit 2

--- JenkinsiOSTest/JenkinsiOSTest/ViewController.swift
+++ JenkinsiOSTest/JenkinsiOSTest/ViewController.swift
@@ -10,26 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
-      @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var checkButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func clickEmail(_ sender: Any) {
-           if let emailField = textField.text {
-               let isValid = isValidEmail(email: emailField)
-                   print(isValid)
-           }
-         
-       }
-       func isValidEmail(email : String) -> Bool {
-           let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
-           let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-           return emailPred.evaluate(with: email)
-       }
-
+        if let emailField = textField.text {
+            let isValid = isValidEmail(email: emailField)
+            print(isValid)
+        }
+        
+    }
+    func isValidEmail(email : String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
 }
 
--- JenkinsiOSTest/JenkinsiOSTestTests/JenkinsiOSTestTests.swift
+++ JenkinsiOSTest/JenkinsiOSTestTests/JenkinsiOSTestTests.swift
@@ -11,46 +11,46 @@ import XCTest
 
 class JenkinsiOSTestTests: XCTestCase {
     var vc : ViewController!
-
+    
     override func setUp() {
         vc = ViewController()
-
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     func testAdd(){
-              let a = 10
-              let b = 50
-              let sum = a + b
-              XCTAssertTrue(sum == 60, "sum")
-          }
-      
-      func testCheck_EmailIsValid(){
-            let email = "test@gmail.com"
-            let isValid = vc.isValidEmail(email: email)
-            XCTAssertTrue(isValid, "Email is Valid")
-          
-        }
+        let a = 10
+        let b = 50
+        let sum = a + b
+        XCTAssertTrue(sum == 60, "sum")
+    }
+    
+    func testCheck_EmailIsValid(){
+        let email = "test@gmail.com"
+        let isValid = vc.isValidEmail(email: email)
+        XCTAssertTrue(isValid, "Email is Valid")
         
-        func testCheck_EmailNotValid(){
-            let email = "test"
-            let isNotValid = vc.isValidEmail(email: email)
-            XCTAssertFalse(isNotValid, "Email is Not Valid")
-        }
-      
+    }
+    
+    func testCheck_EmailNotValid(){
+        let email = "test"
+        let isNotValid = vc.isValidEmail(email: email)
+        XCTAssertFalse(isNotValid, "Email is Not Valid")
+    }
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
