7a0c47075d49dfa14f50be2bf971d7e474ca2332
Nithin Kumar <nkdiyasys@gmail.com>
2020-04-09 10:59:59 +0530

    Commit 1

---
+++ JenkinsiOSTest/JenkinsiOSTest/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  JenkinsiOSTest
+//
+//  Created by Nithin Kumar on 4/9/20.
+//  Copyright © 2020 Diya Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+      @IBOutlet weak var checkButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickEmail(_ sender: Any) {
+           if let emailField = textField.text {
+               let isValid = isValidEmail(email: emailField)
+                   print(isValid)
+           }
+         
+       }
+       func isValidEmail(email : String) -> Bool {
+           let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+           let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+           return emailPred.evaluate(with: email)
+       }
+
+}
+
